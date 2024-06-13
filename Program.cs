88d915fb0f377b618a319d255391c7abf6dd390b
Martin <martin@vese.cz>
2024-06-13 23:13:45 +0200

    Led

--- Program.cs
+++ Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Device.Gpio;
+
+Console.WriteLine("Blinking LED. Press Ctrl+C to end.");
+int pin = 26;
+using var controller = new GpioController();
+controller.OpenPin(pin, PinMode.Output);
+bool ledOn = true;
+while (true)
+{
+    controller.Write(pin, ((ledOn) ? PinValue.High : PinValue.Low));
+    Thread.Sleep(1000);
+    ledOn = !ledOn;
+}
