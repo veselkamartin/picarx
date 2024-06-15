d3de8a71194f959a7037b2a3f8c449716681ab71
Martin <martin@vese.cz>
2024-06-15 22:12:32 +0200

    test

--- Program.cs
+++ Program.cs
@@ -11,7 +11,9 @@ controller.OpenPin(pin, PinMode.Output);
 bool ledOn = true;
 //var motor = new Motor();
 
+Console.WriteLine("Creating bus");
 var _bus = I2cBus.Create(1); // Initialize your I2C bus here
+Console.WriteLine("Creating device");
 var _device = _bus.CreateDevice(0x14);
 
 
@@ -20,16 +22,22 @@ while (true)
     controller.Write(pin, ledOn ? PinValue.High : PinValue.Low);
     Thread.Sleep(1000);
     ledOn = !ledOn;
+    Console.WriteLine("Device test 1");
     _device.Write([0x21, 0x4F, 0x01]);
     Thread.Sleep(1000);
+    Console.WriteLine("Device test 2");
     _device.Write([0x21, 0x1F, 0x01]);
     Thread.Sleep(1000);
+    Console.WriteLine("Device test 3");
     _device.Write([0x21, 0x01, 0x4F]);
     Thread.Sleep(1000);
+    Console.WriteLine("Device test 4");
     _device.Write([0x21, 0x01, 0x1F]);
     Thread.Sleep(1000);
+    Console.WriteLine("Device test 5");
     _device.Write([0x4F, 0x01, 0x21]);
     Thread.Sleep(1000);
+    Console.WriteLine("Device test 6");
     _device.Write([0x1F, 0x01, 0x21]);
     //motor.Wheel(100);
     //Thread.Sleep(1000);
