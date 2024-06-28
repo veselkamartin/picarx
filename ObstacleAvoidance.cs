6cc49abe1842301aaa6f32bfb4f4431bdab51c8f
Martin <martin@vese.cz>
2024-06-28 22:09:52 +0200

    Obstacle

---
+++ ObstacleAvoidance.cs
@@ -0,0 +1,63 @@
+﻿
+namespace PicarX;
+
+public class ObstacleAvoidance
+{
+    static int[] _angles = [30, -30];
+
+    public static void AvoidObstacles(Picarx px)
+    {
+
+        try
+        {
+            while (true)
+            {
+                double distance = Math.Round(px.GetDistance(), 2);
+                Console.WriteLine("distance: " + distance);
+
+                if (distance >= 60)
+                {
+                    px.SetDirServoAngle(0);
+                    px.Forward(50);
+                }
+                else if (distance >= 40)
+                {
+                    px.SetDirServoAngle(0);
+                    px.Forward(20);
+                }
+                else if (distance >= 20)
+                {
+                    var angle = GetRandomLeftOrRight();
+                    px.SetDirServoAngle(angle);
+                    px.Forward(20);
+                    Thread.Sleep(100);
+                    var newDistance = px.GetDistance();
+                    if (newDistance < distance)
+                    {
+                        angle = -angle;
+                        px.SetDirServoAngle(angle);
+                        Thread.Sleep(100);
+                    }
+                }
+                else
+                {
+                    var angle = GetRandomLeftOrRight();
+
+                    px.SetDirServoAngle(angle);
+                    px.Backward(20);
+                    Thread.Sleep(500);
+                }
+            }
+        }
+        finally
+        {
+            px.Stop();
+        }
+
+    }
+
+    private static int GetRandomLeftOrRight()
+    {
+        return _angles[Random.Shared.Next(_angles.Length)];
+    }
+}
--- Program.cs
+++ Program.cs
@@ -2,7 +2,7 @@
 
 class Program
 {
-	static string manual = @"
+    static string manual = @"
 Press keys on keyboard to control PiCar-X!
     w: Forward
     a: Turn left
@@ -15,109 +15,108 @@ Press keys on keyboard to control PiCar-X!
     ctrl+c: Press twice to exit the program
 ";
 
-	static void ShowInfo()
-	{
-		Console.Clear();  // clear terminal window
-		Console.WriteLine(manual);
-	}
+    static void ShowInfo()
+    {
+        Console.Clear();  // clear terminal window
+        Console.WriteLine(manual);
+    }
 
-	static void Main(string[] args)
-	{
-		//ControllerBase.SetTest();
+    static void Main(string[] args)
+    {
+        //ControllerBase.SetTest();
 
-		int pan_angle = 0;
-		int tilt_angle = 0;
-		var px = new Picarx(ControllerBase.GetGpioController(), bus: ControllerBase.CreateI2cBus(1));
+        int pan_angle = 0;
+        int tilt_angle = 0;
+        var px = new Picarx(ControllerBase.GetGpioController(), bus: ControllerBase.CreateI2cBus(1));
 
-		while (true) {
-            var distance = px.GetDistance();
-            Console.WriteLine($"Distance: {distance}");
-			Thread.Sleep(200);
-        }
 
         ShowInfo();
-		try
-		{
-			while (true)
-			{
-				var key = Console.ReadKey();
-				var lowerKey = char.ToLower(key.KeyChar);
+        try
+        {
+            while (true)
+            {
+                var key = Console.ReadKey();
+                var lowerKey = char.ToLower(key.KeyChar);
 
-				if ("wsadikjl".Contains(lowerKey))
-				{
-					if ('w' == lowerKey)
-					{
-						px.SetDirServoAngle(0);
-						px.Forward(80);
-					}
-					else if ('s' == lowerKey)
-					{
-						px.SetDirServoAngle(0);
-						px.Backward(80);
-					}
-					else if ('a' == lowerKey)
-					{
-						px.SetDirServoAngle(-30);
-						px.Forward(80);
-					}
-					else if ('d' == lowerKey)
-					{
-						px.SetDirServoAngle(30);
-						px.Forward(80);
-					}
-					else if ('i' == lowerKey)
-					{
-						tilt_angle += 5;
-						if (tilt_angle > 30)
-							tilt_angle = 30;
-					}
-					else if ('k' == lowerKey)
-					{
-						tilt_angle -= 5;
-						if (tilt_angle < -30)
-							tilt_angle = -30;
-					}
-					else if ('l' == lowerKey)
-					{
-						pan_angle += 5;
-						if (pan_angle > 30)
-							pan_angle = 30;
-					}
-					else if ('j' == lowerKey)
-					{
-						pan_angle -= 5;
-						if (pan_angle < -30)
-							pan_angle = -30;
-					}
+                if ("wsadikjlq".Contains(lowerKey))
+                {
+                    if ('w' == lowerKey)
+                    {
+                        px.SetDirServoAngle(0);
+                        px.Forward(80);
+                    }
+                    else if ('s' == lowerKey)
+                    {
+                        px.SetDirServoAngle(0);
+                        px.Backward(80);
+                    }
+                    else if ('a' == lowerKey)
+                    {
+                        px.SetDirServoAngle(-30);
+                        px.Forward(80);
+                    }
+                    else if ('d' == lowerKey)
+                    {
+                        px.SetDirServoAngle(30);
+                        px.Forward(80);
+                    }
+                    else if ('i' == lowerKey)
+                    {
+                        tilt_angle += 5;
+                        if (tilt_angle > 30)
+                            tilt_angle = 30;
+                    }
+                    else if ('k' == lowerKey)
+                    {
+                        tilt_angle -= 5;
+                        if (tilt_angle < -30)
+                            tilt_angle = -30;
+                    }
+                    else if ('l' == lowerKey)
+                    {
+                        pan_angle += 5;
+                        if (pan_angle > 30)
+                            pan_angle = 30;
+                    }
+                    else if ('j' == lowerKey)
+                    {
+                        pan_angle -= 5;
+                        if (pan_angle < -30)
+                            pan_angle = -30;
+                    }
+                    else if ('q' == lowerKey)
+                    {
+                        ObstacleAvoidance.AvoidObstacles(px);
+                    }
 
-					px.SetCamTiltAngle(tilt_angle);
-					px.SetCamPanAngle(pan_angle);
-					Console.WriteLine();
-					Thread.Sleep(500);
-					//ShowInfo();
-					px.Forward(0);
-					Console.WriteLine();
-					var distance = px.GetDistance();
-					Console.WriteLine($"Distance: {distance}");
-				}
-				else if (key.Key == ConsoleKey.C && key.Modifiers == ConsoleModifiers.Control)
-				{
-					Console.WriteLine("\n Quit");
-					break;
-				}
-				else
-				{
-					Console.WriteLine($"Unknown key {key}");
-				}
-			}
-		}
-		finally
-		{
-			px.SetCamTiltAngle(0);
-			px.SetCamPanAngle(0);
-			px.SetDirServoAngle(0);
-			px.Stop();
-			Thread.Sleep(200);
-		}
-	}
+                    px.SetCamTiltAngle(tilt_angle);
+                    px.SetCamPanAngle(pan_angle);
+                    Console.WriteLine();
+                    Thread.Sleep(500);
+                    //ShowInfo();
+                    px.Forward(0);
+                    Console.WriteLine();
+                    var distance = px.GetDistance();
+                    Console.WriteLine($"Distance: {distance}");
+                }
+                else if (key.Key == ConsoleKey.C && key.Modifiers == ConsoleModifiers.Control)
+                {
+                    Console.WriteLine("\n Quit");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine($"Unknown key {key}");
+                }
+            }
+        }
+        finally
+        {
+            px.SetCamTiltAngle(0);
+            px.SetCamPanAngle(0);
+            px.SetDirServoAngle(0);
+            px.Stop();
+            Thread.Sleep(200);
+        }
+    }
 }
