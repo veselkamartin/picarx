0dcabdcf3b5946e04cc0cb586d637a37ad42682e
Martin <martin@vese.cz>
2024-08-21 23:35:29 +0200

    vc

--- ChatGpt/Camera.cs
+++ ChatGpt/Camera.cs
@@ -10,24 +10,27 @@ public class Camera : IDisposable
 
 	public byte[] GetPictureAsJpeg()
 	{
-//		Iot.Device.Graphics.SkiaSharpAdapter.SkiaSharpAdapter.Register();
-//		VideoConnectionSettings settings = new VideoConnectionSettings(busId: 0, captureSize: (2560, 1920), pixelFormat: VideoPixelFormat.YUYV);
-//using VideoDevice device = VideoDevice.Create(settings);
-//// Capture static image
-//device.Capture("jpg_direct_output.jpg");
+		//ldd libcvextern.so | grep "not found"
+		//sudo apt-get install python3-vtk9
 
-//		// Change capture setting
-//		device.Settings.PixelFormat = VideoPixelFormat.YUV420;
+		//		Iot.Device.Graphics.SkiaSharpAdapter.SkiaSharpAdapter.Register();
+		//		VideoConnectionSettings settings = new VideoConnectionSettings(busId: 0, captureSize: (2560, 1920), pixelFormat: VideoPixelFormat.YUYV);
+		//using VideoDevice device = VideoDevice.Create(settings);
+		//// Capture static image
+		//device.Capture("jpg_direct_output.jpg");
 
-//		// Get image stream, convert pixel format and save to file
-//		var ms = device.Capture();
-//		Color[] colors = VideoDevice.Yv12ToRgb(new MemoryStream( ms), settings.CaptureSize);
-//		var bitmap = VideoDevice.RgbToBitmap(settings.CaptureSize, colors);
-//		bitmap.SaveToFile("yuyv_to_jpg.jpg", Iot.Device.Graphics.ImageFileType.Jpg);
+		//		// Change capture setting
+		//		device.Settings.PixelFormat = VideoPixelFormat.YUV420;
+
+		//		// Get image stream, convert pixel format and save to file
+		//		var ms = device.Capture();
+		//		Color[] colors = VideoDevice.Yv12ToRgb(new MemoryStream( ms), settings.CaptureSize);
+		//		var bitmap = VideoDevice.RgbToBitmap(settings.CaptureSize, colors);
+		//		bitmap.SaveToFile("yuyv_to_jpg.jpg", Iot.Device.Graphics.ImageFileType.Jpg);
 
 		ObjectDisposedException.ThrowIf(_disposedValue, this);
 
-		_capture ??= new VideoCapture();
+		_capture ??= new VideoCapture(-1);
 
 		using var frame = new Mat();
 		_capture.Read(frame);
