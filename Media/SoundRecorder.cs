d64ba624b2b2394af70b3897845b5282368b9b39
Martin <martin@vese.cz>
2024-09-02 21:47:01 +0200

    Comment out

--- Media/SoundRecorder.cs
+++ Media/SoundRecorder.cs
@@ -9,7 +9,7 @@ namespace SmartCar.Media
 			Console.WriteLine("Hello!");
 			//var devices = ALC.GetStringList(GetEnumerationStringList.DeviceSpecifier);
 			//Console.WriteLine($"Devices: {string.Join(", ", devices)}");
-			Console.WriteLine("Liting all devices...");
+			Console.WriteLine("Listing all devices...");
 			var allDevices = ALC.EnumerateAll.GetStringList(GetEnumerateAllContextStringList.AllDevicesSpecifier);
 			foreach (var item in allDevices)
 			{
@@ -34,29 +34,29 @@ namespace SmartCar.Media
 
 			CheckALError("Start");
 
-			ALC.GetInteger(device, AlcGetInteger.MajorVersion, 1, out int alcMajorVersion);
-			ALC.GetInteger(device, AlcGetInteger.MinorVersion, 1, out int alcMinorVersion);
-			string alcExts = ALC.GetString(device, AlcGetString.Extensions);
+			//ALC.GetInteger(device, AlcGetInteger.MajorVersion, 1, out int alcMajorVersion);
+			//ALC.GetInteger(device, AlcGetInteger.MinorVersion, 1, out int alcMinorVersion);
+			//string alcExts = ALC.GetString(device, AlcGetString.Extensions);
 
-			var attrs = ALC.GetContextAttributes(device);
-			Console.WriteLine($"Attributes: {attrs}");
+			//var attrs = ALC.GetContextAttributes(device);
+			//Console.WriteLine($"Attributes: {attrs}");
 
-			string exts = AL.Get(ALGetString.Extensions);
-			string rend = AL.Get(ALGetString.Renderer);
-			string vend = AL.Get(ALGetString.Vendor);
-			string vers = AL.Get(ALGetString.Version);
+			//string exts = AL.Get(ALGetString.Extensions);
+			//string rend = AL.Get(ALGetString.Renderer);
+			//string vend = AL.Get(ALGetString.Vendor);
+			//string vers = AL.Get(ALGetString.Version);
 
-			Console.WriteLine($"Vendor: {vend}, \nVersion: {vers}, \nRenderer: {rend}, \nExtensions: {exts}, \nALC Version: {alcMajorVersion}.{alcMinorVersion}, \nALC Extensions: {alcExts}");
+			//Console.WriteLine($"Vendor: {vend}, \nVersion: {vers}, \nRenderer: {rend}, \nExtensions: {exts}, \nALC Version: {alcMajorVersion}.{alcMinorVersion}, \nALC Extensions: {alcExts}");
 
-			int auxSlot = 0;
-			if (ALC.EFX.IsExtensionPresent(device))
-			{
-				Console.WriteLine("EFX extension is present!!");
-				ALC.EFX.GenEffect(out int effect);
-				ALC.EFX.Effect(effect, EffectInteger.EffectType, (int)EffectType.Reverb);
-				ALC.EFX.GenAuxiliaryEffectSlot(out auxSlot);
-				ALC.EFX.AuxiliaryEffectSlot(auxSlot, EffectSlotInteger.Effect, effect);
-			}
+			//int auxSlot = 0;
+			//if (ALC.EFX.IsExtensionPresent(device))
+			//{
+			//	Console.WriteLine("EFX extension is present!!");
+			//	ALC.EFX.GenEffect(out int effect);
+			//	ALC.EFX.Effect(effect, EffectInteger.EffectType, (int)EffectType.Reverb);
+			//	ALC.EFX.GenAuxiliaryEffectSlot(out auxSlot);
+			//	ALC.EFX.AuxiliaryEffectSlot(auxSlot, EffectSlotInteger.Effect, effect);
+			//}
 
 
 			Console.WriteLine("Available capture devices: ");
@@ -75,6 +75,7 @@ namespace SmartCar.Media
 			CheckALError("Before record");
 			short[] recording = new short[44100 * 4];
 
+			Console.WriteLine($"Recording...");
 			ALC.CaptureStart(captureDevice);
 
 			int current = 0;
@@ -93,6 +94,7 @@ namespace SmartCar.Media
 			ALC.CaptureStop(captureDevice);
 
 			CheckALError("After record");
+			Console.WriteLine($"Recording stopped");
 
 			// Playback the recorded data
 			CheckALError("Before data");
@@ -108,30 +110,30 @@ namespace SmartCar.Media
 			AL.GenSource(out int alSource);
 			AL.Source(alSource, ALSourcef.Gain, 1f);
 			AL.Source(alSource, ALSourcei.Buffer, alBuffer);
-			if (ALC.EFX.IsExtensionPresent(device))
-			{
-				ALC.EFX.Source(alSource, EFXSourceInteger3.AuxiliarySendFilter, auxSlot, 0, 0);
-			}
+			//if (ALC.EFX.IsExtensionPresent(device))
+			//{
+			//	ALC.EFX.Source(alSource, EFXSourceInteger3.AuxiliarySendFilter, auxSlot, 0, 0);
+			//}
 			AL.SourcePlay(alSource);
 
 			Console.WriteLine("Before Playing: " + AL.GetErrorString(AL.GetError()));
 
-			if (ALC.DeviceClock.IsExtensionPresent(device))
-			{
-				long[] clockLatency = new long[2];
-				ALC.DeviceClock.GetInteger(device, GetInteger64.DeviceClock, clockLatency);
-				Console.WriteLine("Clock: " + clockLatency[0] + ", Latency: " + clockLatency[1]);
-				CheckALError(" ");
-			}
+			//if (ALC.DeviceClock.IsExtensionPresent(device))
+			//{
+			//	long[] clockLatency = new long[2];
+			//	ALC.DeviceClock.GetInteger(device, GetInteger64.DeviceClock, clockLatency);
+			//	Console.WriteLine("Clock: " + clockLatency[0] + ", Latency: " + clockLatency[1]);
+			//	CheckALError(" ");
+			//}
 
-			if (AL.SourceLatency.IsExtensionPresent())
-			{
-				AL.SourceLatency.GetSource(alSource, SourceLatencyVector2d.SecOffsetLatency, out var values);
-				AL.SourceLatency.GetSource(alSource, SourceLatencyVector2i.SampleOffsetLatency, out var values1, out var values2, out var values3);
-				Console.WriteLine("Source latency: " + values);
-				Console.WriteLine($"Source latency 2: {Convert.ToString(values1, 2)}, {values2}; {values3}");
-				CheckALError(" ");
-			}
+			//if (AL.SourceLatency.IsExtensionPresent())
+			//{
+			//	AL.SourceLatency.GetSource(alSource, SourceLatencyVector2d.SecOffsetLatency, out var values);
+			//	AL.SourceLatency.GetSource(alSource, SourceLatencyVector2i.SampleOffsetLatency, out var values1, out var values2, out var values3);
+			//	Console.WriteLine("Source latency: " + values);
+			//	Console.WriteLine($"Source latency 2: {Convert.ToString(values1, 2)}, {values2}; {values3}");
+			//	CheckALError(" ");
+			//}
 
 			while ((ALSourceState)AL.GetSource(alSource, ALGetSourcei.SourceState) == ALSourceState.Playing)
 			{
@@ -151,69 +153,69 @@ namespace SmartCar.Media
 					CheckALError(" ");
 				}
 
-				Thread.Sleep(10);
+				Thread.Sleep(50);
 			}
 
 			AL.SourceStop(alSource);
 
 			// Test float32 format extension
-			if (AL.EXTFloat32.IsExtensionPresent())
-			{
-				Console.WriteLine("Testing float32 format extension with a sine wave...");
+			//if (AL.EXTFloat32.IsExtensionPresent())
+			//{
+			//	Console.WriteLine("Testing float32 format extension with a sine wave...");
 
-				float[] sine = new float[44100 * 2];
-				for (int i = 0; i < sine.Length; i++)
-				{
-					sine[i] = MathF.Sin(440 * MathF.PI * 2 * (i / (float)sine.Length));
-				}
+			//	float[] sine = new float[44100 * 2];
+			//	for (int i = 0; i < sine.Length; i++)
+			//	{
+			//		sine[i] = MathF.Sin(440 * MathF.PI * 2 * (i / (float)sine.Length));
+			//	}
 
-				var buffer = AL.GenBuffer();
-				AL.EXTFloat32.BufferData(buffer, FloatBufferFormat.Mono, sine, 44100);
+			//	var buffer = AL.GenBuffer();
+			//	AL.EXTFloat32.BufferData(buffer, FloatBufferFormat.Mono, sine, 44100);
 
-				AL.Listener(ALListenerf.Gain, 0.1f);
+			//	AL.Listener(ALListenerf.Gain, 0.1f);
 
-				AL.Source(alSource, ALSourcef.Gain, 1f);
-				AL.Source(alSource, ALSourcei.Buffer, buffer);
+			//	AL.Source(alSource, ALSourcef.Gain, 1f);
+			//	AL.Source(alSource, ALSourcei.Buffer, buffer);
 
-				AL.SourcePlay(alSource);
+			//	AL.SourcePlay(alSource);
 
-				while ((ALSourceState)AL.GetSource(alSource, ALGetSourcei.SourceState) == ALSourceState.Playing)
-				{
-					Thread.Sleep(10);
-				}
+			//	while ((ALSourceState)AL.GetSource(alSource, ALGetSourcei.SourceState) == ALSourceState.Playing)
+			//	{
+			//		Thread.Sleep(10);
+			//	}
 
-				AL.SourceStop(alSource);
-			}
+			//	AL.SourceStop(alSource);
+			//}
 
-			// Test double format extension
-			if (AL.EXTDouble.IsExtensionPresent())
-			{
-				Console.WriteLine("Testing float64 format extension with a saw wave...");
+			//// Test double format extension
+			//if (AL.EXTDouble.IsExtensionPresent())
+			//{
+			//	Console.WriteLine("Testing float64 format extension with a saw wave...");
 
-				double[] saw = new double[44100 * 2];
-				for (int i = 0; i < saw.Length; i++)
-				{
-					var t = i / (double)saw.Length * 440;
-					saw[i] = t - Math.Floor(t);
-				}
+			//	double[] saw = new double[44100 * 2];
+			//	for (int i = 0; i < saw.Length; i++)
+			//	{
+			//		var t = i / (double)saw.Length * 440;
+			//		saw[i] = t - Math.Floor(t);
+			//	}
 
-				var buffer = AL.GenBuffer();
-				AL.EXTDouble.BufferData(buffer, DoubleBufferFormat.Mono, saw, 44100);
+			//	var buffer = AL.GenBuffer();
+			//	AL.EXTDouble.BufferData(buffer, DoubleBufferFormat.Mono, saw, 44100);
 
-				AL.Listener(ALListenerf.Gain, 0.1f);
+			//	AL.Listener(ALListenerf.Gain, 0.1f);
 
-				AL.Source(alSource, ALSourcef.Gain, 1f);
-				AL.Source(alSource, ALSourcei.Buffer, buffer);
+			//	AL.Source(alSource, ALSourcef.Gain, 1f);
+			//	AL.Source(alSource, ALSourcei.Buffer, buffer);
 
-				AL.SourcePlay(alSource);
+			//	AL.SourcePlay(alSource);
 
-				while ((ALSourceState)AL.GetSource(alSource, ALGetSourcei.SourceState) == ALSourceState.Playing)
-				{
-					Thread.Sleep(10);
-				}
+			//	while ((ALSourceState)AL.GetSource(alSource, ALGetSourcei.SourceState) == ALSourceState.Playing)
+			//	{
+			//		Thread.Sleep(10);
+			//	}
 
-				AL.SourceStop(alSource);
-			}
+			//	AL.SourceStop(alSource);
+			//}
 
 			Console.WriteLine("Goodbye!");
 
